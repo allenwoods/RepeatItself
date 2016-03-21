a030b60d7c9c2bbd267e98654ae43ccfaf77a5dc
allenwoods <allen.woods@foxmail.com>
2016-03-21 20:40:11 +0800

    init commit

--- repeatitself.py
+++ repeatitself.py
@@ -1,4 +1,16 @@
 # Author: Allen Woods
 # Date: 2016-03-21
 
+import os
+import sys
 
+
+def repeat(itself):
+    src = open(itself).read()
+    print(src)
+
+
+if __name__ == '__main__':
+    cwd = os.getcwd()
+    itself = os.path.join(cwd, sys.argv[0])
+    repeat(itself)
